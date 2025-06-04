967d8ae5da95e8006f718317dfcb9b80c4ce0de5
raghav2606 <raghav26697@gmail.com>
2025-06-04 19:18:29 +0530

    New Changes

--- ingestion.py
+++ ingestion.py
@@ -1,2 +1,10 @@
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+
+load_dotenv()
+
+
 if __name__=="__main__":
     print('Ingesting...')
